4795cb31a9b9a96d549223b4d189925e18b93b6b
Whit3Tig3R <b0y.4l0n3@yahoo.com>
2016-02-06 01:43:01 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {54213663,160553465},--Sudo users
+    sudo_users = {181803152,160553465},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
